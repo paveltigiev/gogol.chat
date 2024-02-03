e4e5d89b5049fcfafd4fae42d6005a7a03a12968
Pavel Tigiev <paulo.tigiev@gmail.com>
2024-02-03 11:34:57 +0300

    fix redirect to waiting

--- src/App.vue
+++ src/App.vue
@@ -4,38 +4,43 @@ import { supabase } from './supabase'
 import { useCommonsStore } from './stores/commons'
 import { useUserStore } from './stores/user'
 import AuthView from './views/AuthView.vue'
+import WaitingView from './views/WaitingView.vue'
 
 const commonsStore = useCommonsStore()
 const overlay = computed(() => commonsStore.overlay)
 
 const userStore = useUserStore()
 const user = computed(() => userStore.user )
-const session = ref()
+const status = ref('')
 
 onMounted(async () => {
   await supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
+    userStore.user = data.session? data.session.user : null
   })
 
   await supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
+    userStore.user = _session? _session.user : null
   })
 
-  userStore.user = session.value ? session.value.user : null
-
-  try {
-    const { data: { user } } = await supabase.auth.getUser()
-    console.log('getUser')
-    console.log(user)
-  } catch (error) {
-    alert(error.message)
+  if (user.value) {
+    try {
+      const { data: user_profile } = await supabase
+        .from('user_profiles')
+        .select('status')
+        .eq('user_id', user.value.id)
+        .single()
+      status.value = user_profile.status
+    } catch (error) {
+      alert(error.message)
+    }
   }
 })
 </script>
 
 <template>
   <div class="overlay" :class="[overlay ? 'active' : 'inactive']"></div>
-  <RouterView v-if="session" :session="session" />
+  <RouterView v-if="user && status.charAt(0) == 'a'" />
+  <WaitingView v-if="user && status.charAt(0) == 'w'" />
   <AuthView v-else />
 </template>
 
--- src/views/WaitingView.vue
+++ src/views/WaitingView.vue
@@ -1,14 +1,91 @@
-<template>
-  <div>
-    <h1>You are in the line</h1>
-    <button>Log out</button>
-  </div>
-</template>
-
 <script setup>
+import { ref } from 'vue'
+import { supabase } from '../supabase'
 
 </script>
 
-<style lang="scss" scoped>
+<template>
+  <div class="auth">
+    <div class="auth__logo"></div>
+    <div>
+      <h1 class="auth__title">You are in the line</h1>
 
+      <button type="button" class="logoutButton" @click="logOut">
+        Log out
+      </button>
+    </div>
+    <footer class="footer">
+      <a href="https://gogol.chat/terms" target="_blank">Terms of use</a>
+      |
+      <a href="https://gogol.chat/policy" target="_blank">Privacy policy</a>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.auth {
+  width: 400px;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 30px 40px 20px;
+  justify-content: space-between;
+  text-align: center;
+
+  &__logo {
+    width: 34px;
+    height: 34px;
+    background: url('../assets/img/ai.png') center center no-repeat;
+    background-size: contain;
+    border-radius: 50%;
+    margin: 0 auto;
+  }
+  &__title {
+    font-weight: bold;
+    font-size: 32px;
+    margin: 0 0 20px;
+    text-align: center;
+    letter-spacing: 0.04em;
+  }
+
+
+  .logoutButton {
+    margin-top: 24px;
+    position: relative;
+    display: flex;
+    padding: 0 8px;
+    background: transparent;
+    align-items: center;
+    width: 100%;
+    font-size: 16px;
+    font-family: inherit;
+    height: 52px;
+    border: 1px solid #c2c8d0;
+    border-radius: 6px;
+    color: #2d333a;
+    cursor: pointer;
+    outline: 0;
+    transition: box-shadow .15s ease-in-out,background-color .15s ease-in-out;
+    justify-content: center;
+
+    &:hover {
+      box-shadow: inset 0 0 0 150px rgba(0,0,0,.1);
+    }
+
+  }
+  .footer {
+    font-size: 14px;
+    display: flex;
+    gap: 10px;
+    justify-content: center;
+
+    a {
+      color: #613dc4;
+      text-decoration: none;
+      letter-spacing: -0.02em;
+    }
+  }
+}
 </style>
